37c6337025fe862fd242ef7527d994cc04a2824e
Benjamin Russell <benjaminrobertrussell@gmail.com>
2021-10-26 23:10:30 -0400

    small update

--- client/src/components/Welcome/Welcome.js
+++ client/src/components/Welcome/Welcome.js
@@ -3,14 +3,45 @@ import styled from 'styled-components';
 
 const Welcome = () => {
     return(
-        <Wrapper>From Welcome</Wrapper>
+        <Wrapper>
+            <Col>
+                <Title>Escape</Title>
+                <Text>Welcome to my Roguelike project!</Text>
+                <Text>Temporarily named "Escape".</Text>
+            </Col>
+
+        </Wrapper>
     )
 }
 
 const Wrapper = styled.div`
     display: flex;
+    /* justify-content: center; */
+    /* align-items: center; */
+    height: 100%;
+    width: 100%;
+    `
+
+const Col = styled.div`
+    display: flex;
+    flex-direction: column;
+    border: 1px solid white;
+    border-radius: 7px;
+    padding: 20px;
+    height: 100%;
+    width: 100%;
+`
+
+const Title = styled.div`
+    display: flex;
     justify-content: center;
-    align-items: center;
+    font-size: 2em;
+    font-weight: 900;
+`
+
+const Text = styled.div`
+    display: flex;
+    justify-content: center;
 `
 
 export default Welcome;
